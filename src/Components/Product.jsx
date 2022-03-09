4dceaa5c2a86ed24c618818238a9f1d28a48e882
Ahmed Saleem Shaikh <98047091+AhmedShaykh@users.noreply.github.com>
2022-03-09 23:24:12 +0500

    It's Time To Push !

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import Shoes from './../Shoes.json';
 
 function Product() {
+    console.log(Shoes);
+    console.log(Object.keys(Shoes));
     return (
         <div>
             <h1>
                 Welcome To Product Page
             </h1>
+            <div>
+                {Object.keys(Shoes).map(keyName => {
+                    const shoe = Shoes[keyName];
+                    return (
+                        <div key={keyName}>
+                            <h1>
+                                {keyName}
+                            </h1>
+                            <h2>
+                                {shoe.name}
+                            </h2>
+                            <img src={shoe.img} height={300}
+                            alt='shoe' />
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
